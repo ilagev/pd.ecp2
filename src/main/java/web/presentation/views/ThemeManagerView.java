bc2f9af74a01c5977c2c9f6814a09552ea402f35
Iván Lage Vázquez <ilagev@gmail.com>
2015-10-20 21:21:52 +0200

    Fixed theme and vote views: printing ", " when there isn't next element

--- src/main/java/web/presentation/views/ThemeManagerView.java
+++ src/main/java/web/presentation/views/ThemeManagerView.java
@@ -7,8 +7,13 @@ public class ThemeManagerView implements View {
     @Override
     public void show(Model model) {
         System.out.print("Theme Manager Page\n\t[");
+        int size = model.getMap().keySet().size(), i = 0;
         for (String name : model.getMap().keySet()) {
-            System.out.print(model.getMap().get(name) + ", ");
+            System.out.print(model.getMap().get(name));
+            if (i < size - 1) {
+                System.out.print(", ");
+                i++;
+            }
         }
         System.out.println("]");
     }
--- src/main/java/web/presentation/views/VotingView.java
+++ src/main/java/web/presentation/views/VotingView.java
@@ -7,8 +7,13 @@ public class VotingView implements View {
     @Override
     public void show(Model model) {
         System.out.print("Voting Page\n\t[");
+        int size = model.getMap().keySet().size(), i = 0;
         for (String name : model.getMap().keySet()) {
-            System.out.print(model.getMap().get(name).toString() + ", ");
+            System.out.print(model.getMap().get(name).toString());
+            if (i < size - 1) {
+                System.out.print(", ");
+                i++;
+            }
         }
         System.out.println("]");
     }
